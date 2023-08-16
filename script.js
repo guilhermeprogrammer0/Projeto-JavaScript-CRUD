05955fceb5286577432695dccef7fee2ff310576
Guilherme Souza <guilherme003souza@gmail.com>
2023-08-16 19:24:44 -0300

    Enviando ao github

---
+++ script.js
@@ -0,0 +1,66 @@
+class Cliente{
+    constructor(nome,cpf,data){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.data = data;
+    }
+}
+let tabela_dados = document.querySelector("#tabela");
+let btnCadastrar = document.querySelector("#cadastrar");
+btnCadastrar.addEventListener('click',Cadastrar);
+var lista_clientes = [];
+var posicao = '';
+function Cadastrar(){
+     let nome = document.querySelector("#nome");
+     let cpf = document.querySelector("#cpf");
+     let data = document.querySelector("#data");
+    if(nome.value!='' && cpf.value !='' && data.value!=''){
+         cliente = new Cliente(nome.value,cpf.value,data.value);
+        if(posicao == ''){
+            lista_clientes.push(cliente);
+        }
+        else{
+            lista_clientes[posicao] = cliente;
+            posicao = '';
+        }
+        tabela_dados.innerHTML = Listar(lista_clientes);
+        nome.value = '';
+        cpf.value = '';
+        data.value = '';
+    }
+    else{
+        alert('Preencha os campos!');
+    }
+}
+function Listar(lista){
+    var auxHtml = '';
+    lista.forEach((cliente,indice)=>{
+        auxHtml += `
+        <tr>
+        <td> ${cliente.nome} </td>
+        <td> ${cliente.cpf} </td>
+        <td> ${cliente.data} </td>
+        <td>  <a href="#" rel="${indice}" class="btn btn-warning btnAlterar"> Alterar  </td>
+        <td>  <a href="#" rel="${indice}" class="btn btn-danger btnExcluir"> Excluir  </td>     
+        </tr>
+        `
+    })
+    return auxHtml;
+}
+tabela_dados.addEventListener('click',Acoes)
+function Acoes(event){
+    posicao = event.target.rel;
+    if(event.target.classList.contains('btnAlterar')){
+        document.getElementById("nome").value = lista_clientes[posicao].nome;
+        document.getElementById("cpf").value = lista_clientes[posicao].cpf;
+        document.getElementById("data").value = lista_clientes[posicao].data;
+    }
+    else if(event.target.classList.contains("btnExcluir")){
+        let confirmar = window.confirm("Deseja mesmo excluir?");
+        if(confirmar == true){
+            lista_clientes.splice(posicao,1);
+        tabela_dados.innerHTML = Listar(lista_clientes);
+        }
+    }
+}
+

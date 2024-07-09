3080dc1d3204329bb985537d523f81cc1aea5a8a
Guilherme <guilherme003souza@gmail.com>
2024-07-08 22:01:35 -0300

    Projeto sendo melhorado

--- script.js
+++ script.js
@@ -7,71 +7,82 @@ class Cliente{
 }
 let tabela_dados = document.querySelector("#tabela");
 let btnCadastrar = document.querySelector("#cadastrar");
-btnCadastrar.addEventListener('click',Cadastrar);
 var lista_clientes = [];
 var posicao = '';
-function Cadastrar(){
-     let nome = document.querySelector("#nome");
-     let cpf = document.querySelector("#cpf");
-     let data = document.querySelector("#data");
-     if(localStorage.getItem("listaClientes") !=null){
-        lista_clientes = JSON.parse(localStorage.getItem("listaClientes"))
-     }
-    if(nome.value!='' && cpf.value !='' && data.value!=''){
-         cliente = new Cliente(nome.value,cpf.value,data.value);
-        if(posicao == ''){
+var nome = document.querySelector("#nome");
+var cpf = document.querySelector("#cpf");
+var data = document.querySelector("#data");
+var campos = document.querySelectorAll(".campos");
+btnCadastrar.addEventListener("click",()=>{
+    btnCadastrar.innerHTML = 'Cadastrar';
+    btnCadastrar.classList.remove("btn-warning");
+    campos.forEach((campo)=>{
+        if(campo.value ==''){
+            campo.classList.add("campoVazio");
+        }
+        else{
+            campo.classList.remove("campoVazio");
+        }
+    })
+        if(nome.value ==='' || cpf.value ==='' || data.value ===''){
+            alert("Preencha todos os campos!")
+        }
+        else{
+        cliente = new Cliente(nome.value,cpf.value,data.value);
+        if(localStorage.getItem("ListaClientes")!=null){
+            lista_clientes = JSON.parse(localStorage.getItem("ListaClientes"));
+        }
+        if(posicao==''){
             lista_clientes.push(cliente);
         }
         else{
-            lista_clientes[posicao] = cliente; 
+            lista_clientes[posicao] = cliente;
             posicao = '';
         }
-        localStorage.setItem("listaClientes",JSON.stringify(lista_clientes));
         nome.value = '';
         cpf.value = '';
         data.value = '';
+        localStorage.setItem("ListaClientes",JSON.stringify(lista_clientes));
+        carregar();
     }
-    else{
-        alert('Preencha os campos!');
-    }
-    carregar();
-}
-function Listar(lista){
-    var auxHtml = '';
-    lista.forEach((cliente,indice)=>{
-        auxHtml += `
+});
+function listar(lista){
+    let auxHtml = '';
+    lista.forEach((c,indice)=>{
+        auxHtml +=`
         <tr>
-        <td> ${cliente.nome} </td>
-        <td> ${cliente.cpf} </td>
-        <td> ${cliente.data} </td>
-        <td>  <a href="#" rel="${indice}" class="btn btn-warning btnAlterar"> Alterar  </td>
-        <td>  <a href="#" rel="${indice}" class="btn btn-danger btnExcluir"> Excluir  </td>     
+            <td> ${c.nome} </td>
+            <td> ${c.cpf} </td>
+            <td> ${c.data} </td>
+            <td> <a href="#" class="btn btn-warning btnAlterar" rel="${indice}"> Alterar </a></td>
+            <td> <a href="#" class="btn btn-danger btnExcluir" rel="${indice}"> Excluir </a></td>
         </tr>
-        `
-    })
+        `   
+    });
     return auxHtml;
 }
-tabela_dados.addEventListener('click',Acoes)
+tabela_dados.addEventListener("click",Acoes);
 function Acoes(event){
-    if(localStorage.getItem("listaClientes") !=null){
-        lista_clientes = JSON.parse(localStorage.getItem("listaClientes"))
-     }
     posicao = event.target.rel;
-    if(event.target.classList.contains('btnAlterar')){
-        document.getElementById("nome").value = lista_clientes[posicao].nome;
-        document.getElementById("cpf").value = lista_clientes[posicao].cpf;
-        document.getElementById("data").value = lista_clientes[posicao].data;
+    if(localStorage.getItem("ListaClientes")!=null){
+        lista_clientes = JSON.parse(localStorage.getItem("ListaClientes"));
+    }
+    if(event.target.classList.contains("btnAlterar")){
+        btnCadastrar.innerHTML = 'Editar';
+        btnCadastrar.classList.add("btn-warning");
+        nome.value = lista_clientes[posicao].nome;
+        cpf.value = lista_clientes[posicao].cpf;
+        data.value = lista_clientes[posicao].data;
     }
     else if(event.target.classList.contains("btnExcluir")){
-        let confirmar = window.confirm("Deseja mesmo excluir?");
+        let confirmar = confirm("Deseja mesmo excluir?");
         if(confirmar == true){
             lista_clientes.splice(posicao,1);
-            localStorage.setItem("listaClientes",JSON.stringify(lista_clientes));
+            localStorage.setItem("ListaClientes",JSON.stringify(lista_clientes));
             carregar();
-        }
+        }  
     }
-    
 }
 function carregar(){
-    tabela_dados.innerHTML = Listar(JSON.parse(localStorage.getItem("listaClientes")));
+    tabela_dados.innerHTML = listar(JSON.parse(localStorage.getItem("ListaClientes")))
 }
